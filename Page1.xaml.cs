2838393c678ec9df81fd5958cb79867cfd574069
ПК <@DESKTOP-HHT7APO>
2020-12-10 03:24:17 +0500

    Добавьте файлы проекта.

---
+++ Page1.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UchP11
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            var tovars = baseEntities.GetContext().Tovar.ToList();
+            ListViewTovar.ItemsSource = tovars;
+        }
+        private void ButtonEditClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Page2((sender as Button).DataContext as Tovar));
+        }
+
+        private void updateTovars()
+        {
+        }
+
+        private void ButtonAddClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Page2(null));
+        }
+        private void ButtonDeleteClick(object sender, RoutedEventArgs e)
+        {
+            var tovarsForRemoving = ListViewTovar.SelectedItems.Cast<Tovar>().ToList();
+            if (MessageBox.Show($"Удалить выбранные Вами товары?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    baseEntities.GetContext().Tovar.RemoveRange(tovarsForRemoving);
+                    baseEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Удаление прошло успешно!");
+
+                    ListViewTovar.ItemsSource = baseEntities.GetContext().Tovar.ToList();
+                }
+                catch
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                baseEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ListViewTovar.ItemsSource = baseEntities.GetContext().Tovar.ToList();
+            }
+        }
+        private void ButtonHistoryClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Page3());
+        }
+    }
+}
---
+++ Page2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UchP11
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        private Tovar tovar = new Tovar();
+        public Page2(Tovar selectedtovar)
+        {
+            InitializeComponent();
+            DataContext = tovar;
+            ComboBox1.ItemsSource = baseEntities.GetContext().Proizvoditel.ToList();
+            if (selectedtovar != null)
+                tovar = selectedtovar;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+                baseEntities.GetContext().Tovar.Add(tovar);
+                try
+                {
+                    baseEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!");
+                    Manager.MainFrame.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+        }
+    }
+}
